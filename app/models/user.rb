91b789dd140e46d5253314563c39fb7190a7b5ba
yashiki2049 <poppy.red.2003@gmail.com>
2020-11-24 20:53:12 +0900

    モデルのバリデーションとエラーメッセージの表示の追加 他
    ・モデルにバリデーションとエラーメッセージの表示を記述
    ・他 ビューファイルの入れ間違いがあったので直し

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :nickname, presence: true 
   validates :birth_date, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i , message: "Include both letters and numbers"}
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ , message: "Full-width characters"}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ , message: "Full-width characters"}
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ , message: "Full-width katakana characters"}
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ , message: "Full-width katakana characters"}
 
   # has_many :items
   # has_one :item_purchases
