c12953a6631acd28646dd703c30d9b485578b7d5
yashiki2049 <poppy.red.2003@gmail.com>
2020-11-25 16:56:11 +0900

    userモデルのバリデーション修正
    ・with_optionsを使用した記述に修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :birth_date, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers' }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
 
+  with_options presence: true do
+    validates :nickname
+    validates :birth_date
+        
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
+      validates :last_name
+      validates :first_name
+    end
+        
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+  end
   # has_many :items
   # has_one :item_purchases
 end
