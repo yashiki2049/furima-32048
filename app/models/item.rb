449cb7420db46c165885d7eb8fb6a05b7dc90f8d
yashiki2049 <poppy.red.2003@gmail.com>
2020-11-26 12:06:29 +0900

    ActiveHashモデルの作成 他
    ・ActiveHashモデルの作成
    ・アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,19 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_burden
+  belongs_to :prefecture
+  belongs_to :shipping_day
+
+  with_options numericality: { other_than: 0 } do
+    validates:category_id
+    validates:status_id
+    validates:shipping_burden_id
+    validates:prefecture_id
+    validates:shipping_day_id
+  end
 end
