200fe1c6ada2d38612e47180590609681308bd7e
yashiki2049 <poppy.red.2003@gmail.com>
2020-11-26 17:45:41 +0900

    商品情報保存機能の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,20 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def item_params
+    params.require(:item).permit(:image,:name,:info,:category_id,:status_id,:shipping_burden_id,:prefecture_id,:shipping_day_id,:price).merge(user_id: current_user.id)
+  end
 end
