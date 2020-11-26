214b6acb8540d1b65f14b060117d58746c443b48
yashiki2049 <poppy.red.2003@gmail.com>
2020-11-26 15:59:55 +0900

    itemモデルのバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,11 +9,21 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_day
 
-  with_options numericality: { other_than: 0 } do
-    validates:category_id
-    validates:status_id
-    validates:shipping_burden_id
-    validates:prefecture_id
-    validates:shipping_day_id
+  with_options presence: true do
+    validates :name
+    validates :info
+    validates :price, numericality: true,
+                      numericality: {
+                      greater_than_or_equal_to: 333,
+                      less_than_or_equal_to: 9999999 
+                    }
+
+    with_options numericality: { other_than: 0 } do
+      validates :category_id
+      validates :status_id
+      validates :shipping_burden_id
+      validates :prefecture_id
+      validates :shipping_day_id
+    end
   end
 end
