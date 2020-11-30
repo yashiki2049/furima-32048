806beb7a367461dbfa590c72b213a4ef89528e83
yashiki2049 <poppy.red.2003@gmail.com>
2020-11-30 15:01:44 +0900

    Rubocopの実行と自動修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :items
   has_many :user_items
   has_one :item_purchases
