36ed94c755582fa6919e92208db28c9226313c75
yashiki2049 <poppy.red.2003@gmail.com>
2020-11-30 16:37:27 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2020_11_29_122417) do
     t.integer "prefecture_id", null: false
     t.integer "shipping_day_id", null: false
     t.integer "price", null: false
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
