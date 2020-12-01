9eb587f08ec3bf0fc5ea9d5202295626e7dfb7e2
yashiki2049 <poppy.red.2003@gmail.com>
2020-12-01 12:16:43 +0900

    売却済み商品の編集画面へ遷移制限
    ・売却済商品の編集画面へ遷移しようとするとトップページへ遷移する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:show, :index]
+  before_action :move_to_index_soldout, only: [:edit]
   before_action :move_to_index, only: [:edit]
-  before_action :authenticate_user!, only: [:new]
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -37,8 +38,16 @@ class ItemsController < ApplicationController
   end
 
   private
+  
   def item_params
     params.require(:item).permit(:image, :name, :info, :category_id, :status_id, :shipping_burden_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index_soldout
+    @item = Item.find(params[:id])
+    unless @item.user_item.blank?
+    redirect_to action: :index
+    end
   end
 
   def move_to_index
