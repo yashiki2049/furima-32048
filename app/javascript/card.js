4a28c5f4c5863eb7fc6912ca46ff168081991a00
yashiki2049 <poppy.red.2003@gmail.com>
2020-12-05 16:18:01 +0900

    カード情報のトークン化を行うjsファイルの作成

---
+++ app/javascript/card.js
@@ -0,0 +1,36 @@
+const pay = () => {
+  Payjp.setPublicKey("pk_test_e7c98f86142df1e018e48c4b");
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("フォーム送信時にイベント発火") //消す
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("item_purchase[number]"),
+      cvc: formData.get("item_purchase[cvc]"),
+      exp_month: formData.get("item_purchase[exp_month]"),
+      exp_year: `20${formData.get("item_purchase[exp_year]")}`,
+    };
+
+      Payjp.createToken(card, (status, response) => {
+        if (status == 200) {
+          const token = response.id;
+          const renderDom = document.getElementById("charge-form");
+          const tokenObj = `<input value=${token} name='token' type="hidden">`;
+          renderDom.insertAdjacentHTML("beforeend", tokenObj);
+        }
+
+        document.getElementById("card-number").removeAttribute("name");
+        document.getElementById("card-cvc").removeAttribute("name");
+        document.getElementById("card-exp-year").removeAttribute("name");
+        document.getElementById("card-exp-year").removeAttribute("name");
+
+        document.getElementById("charge-form").submit();
+      });
+    });
+  };
+
+window.addEventListener("load", pay);
