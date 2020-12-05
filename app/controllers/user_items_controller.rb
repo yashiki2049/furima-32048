347f4f99f5cbb665f0b01d5cefb2cfaeaefacfef
yashiki2049 <poppy.red.2003@gmail.com>
2020-12-05 14:45:04 +0900

    購入者情報・商品購入情報の保存を実装　他
    ・Formオブジェクトを作成（クラス名：ItemPurchase）
    　購入者の個人情報をaddressesテーブルへ保存
    　購入情報をuser_itemsテーブルへ保存
    　バリデーションの記述
    ・user_itemsコントローラにcreateアクションとストロングパラメーターの記述
    ・購入情報入力画面にエラーメッセージが出るようにrenderの記述

--- app/controllers/user_items_controller.rb
+++ app/controllers/user_items_controller.rb
@@ -1,10 +1,24 @@
 class UserItemsController < ApplicationController
-  
-  def index
+   def index
     @item = Item.find(params[:item_id])
-  end
+    @item_purchase = ItemPurchase.new
+   end
 
-  def create
-  end
+   def create
+      @item = Item.find(params[:item_id])
+      @item_purchase = ItemPurchase.new(purchase_params)
+      if @item_purchase.valid?
+        @item_purchase.save
+        redirect_to root_path
+      else
+        render :index 
+      end
+   end
+
+   private
+
+   def purchase_params
+    params.require(:item_purchase).permit(:postal_code, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id ,item_id: params[:item_id])
+   end
 
 end
---
+++ app/models/item_purchase.rb
@@ -0,0 +1,34 @@
+class ItemPurchase
+
+  include ActiveModel::Model
+  attr_accessor :postal_code,
+                :prefecture_id,
+                :city,
+                :block,
+                :building,
+                :phone_number,
+                :user_item_id,
+                :user_id,
+                :item_id
+
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'Input correctly' }
+    validates :prefecture_id, numericality: { other_than: 0, message: 'Select' }
+    validates :city
+    validates :block
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'Input only number' }
+  end
+
+  def save
+    useritem = UserItem.create(user_id: user_id,
+                               item_id: item_id)
+
+    Address.create(postal_code: postal_code,
+                   prefecture_id: prefecture_id,
+                   city: city,
+                   block: block,
+                   building: building,
+                   phone_number: phone_number,
+                   user_item_id: useritem.id)
+  end
+end
