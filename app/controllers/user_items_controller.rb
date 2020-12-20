90aa94e1133b2a2ce489f703e96880c96afb95ab
yashiki2049 <poppy.red.2003@gmail.com>
2020-12-20 14:53:24 +0900

    EC2上で購入をできるように設定の見直し
    ・マスターキーにPAYJPの秘密鍵と公開鍵を設定
    ・コントローラーの秘密鍵をマスターキーからもってくるように記述
    ・購入のためのJSの公開鍵をマスターキーからもってくるように記述

--- app/controllers/user_items_controller.rb
+++ app/controllers/user_items_controller.rb
@@ -33,7 +33,7 @@ class UserItemsController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp.api_key = Rails.application.credentials.payjp[:PAYJP_SECRET_KEY]
     Payjp::Charge.create(
       amount: @item.price,
       card: params[:token],
