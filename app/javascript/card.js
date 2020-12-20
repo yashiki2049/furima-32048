8a59cd6ea5206d5888811bf44a2301d877787fc4
yashiki2049 <poppy.red.2003@gmail.com>
2020-12-20 16:24:24 +0900

    JS修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -19,7 +19,7 @@ debugger
         if (status == 200) {
           const token = response.id;
           const renderDom = document.getElementById("charge-form");
-          const tokenObj = `<input value=${token} name='token' type="hidden">`;
+          const tokenObj = `<input value=${token} name='token' type='hidden'>`;
           renderDom.insertAdjacentHTML("beforeend", tokenObj);
         }
 
